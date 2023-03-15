ec1faec98f5536bbdad080bfbf34a5e3ffc1f8e1
vivek <vivek@DESKTOP-KKFLPJD.lan>
2023-03-15 12:08:20 +0400

    Changes in config properties file for excelRead

--- GroceryApp/src/test/java/testCases/LoginPageTestCases.java
+++ GroceryApp/src/test/java/testCases/LoginPageTestCases.java
@@ -62,8 +62,8 @@ public class LoginPageTestCases extends baseClass { // inheritance
 	@Test
 	public void verifyValidCredentials() throws IOException {
 		lp = new LoginPage(driver);
-		lp.enterUsername(ExcelRead.readStringData("Sheet1", 1, 0));
-		lp.enterPassword(ExcelRead.readStringData("Sheet1", 1, 1));
+		lp.enterUsername(ExcelRead.readStringData((prop.getProperty("DataProviderSheet")), 1, 0));
+		lp.enterPassword(ExcelRead.readStringData((prop.getProperty("DataProviderSheet")), 1, 1));
 		lp.clickLogin();
 		String actualResult = lp.URLAfterSignIn();
 		String expectedResult =Constant.ADMINURL;
